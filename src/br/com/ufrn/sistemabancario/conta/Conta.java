e74cf3c763ff14ed0014933c2c373b359161d0dd
LaelRodrigues <tecrodrigues@ufrn.edu.br>
2021-07-20 22:57:18 -0300

    criando a classe Conta com o construtor padrão e o parametrizado

---
+++ src/br/com/ufrn/sistemabancario/conta/Conta.java
@@ -0,0 +1,17 @@
+package br.com.ufrn.sistemabancario.conta;
+
+public class Conta {
+	
+	private int numero;
+	private float saldo;
+	
+	public Conta() {}
+	
+	public Conta(int numero, float saldo) {
+		super();
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	
+}
