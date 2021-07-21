e1889e6aac7858ab4ad60d5ae4f2b62c09014f4a
LaelRodrigues <tecrodrigues@ufrn.edu.br>
2021-07-21 09:59:32 -0300

    adicionando os métodos gets, sets, equals e hashCode a classe Conta

--- src/br/com/ufrn/sistemabancario/conta/Conta.java
+++ src/br/com/ufrn/sistemabancario/conta/Conta.java
@@ -12,6 +12,45 @@ public class Conta {
 		this.numero = numero;
 		this.saldo = saldo;
 	}
-	
-	
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numero;
+		result = prime * result + Float.floatToIntBits(saldo);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numero != other.numero)
+			return false;
+		if (Float.floatToIntBits(saldo) != Float.floatToIntBits(other.saldo))
+			return false;
+		return true;
+	}
 }
